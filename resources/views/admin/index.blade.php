baa7c0a277a54e9a8706fd8a25edbc52fdcf3965
javierBorjas <cristman11@gmail.com>
2026-04-26 22:58:59 -0300

    Controllador admin, Layout principal y ajustes globales

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="page-heading">
+        <h3>Dashboard</h3>
+    </div>
+    <div class="page-content">
+        <p>Welcome to the admin dashboard!</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\Admin::class, 'index'])->name('home')->middleware('auth');
+Route::get('/admin', [App\Http\Controllers\Admin::class, 'index'])->name('admin.index')->middleware('auth');
